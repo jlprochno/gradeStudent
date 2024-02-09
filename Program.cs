3568540f9a9abb4203374b86e30dcb59dd73db5f
Jenyffer Prochno <jlprochno@gmail.com>
2024-02-09 09:59:44 -0300

    Publishing

---
+++ Program.cs
@@ -0,0 +1,21 @@
+﻿namespace gradeStudent;
+class Program
+{
+    static void Main(string[] args)
+    {  
+        bool loop = true;
+
+        while(loop)
+        {
+            Console.WriteLine("Digite a nota do aluno:");
+            int grade = Convert.ToInt32(Console.ReadLine());
+            
+            if(grade >= 7)
+            loop = false;
+            else
+                Console.WriteLine("Por gentileza, tente novamente!");
+        }
+        Console.WriteLine("Parabéns você foi aprovado!");
+    }
+}
+
